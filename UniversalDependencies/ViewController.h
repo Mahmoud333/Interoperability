f2a0fb6e1e76df5d213c96e82e22a122ad6a2579
Mahmoud <Mahmoud_Smgl@hotmail.com>
2019-12-20 12:55:34 +0200

    Initial Commit

---
+++ UniversalDependencies/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UniversalDependencies
+//
+//  Created by Mahmoud Hamad on 12/20/19.
+//  Copyright © 2019 Mahmoud Hamad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UniversalDependencies/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  UniversalDependencies
+//
+//  Created by Mahmoud Hamad on 12/20/19.
+//  Copyright © 2019 Mahmoud Hamad. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
